f314565e63ba2ef221059d331f6d8d1df6ee25bb
jinfakeji168 <jinfakeji168@gmail.com>
2024-10-24 12:48:23 +0800

    fix

--- eslint.config.js
+++
@@ -1,9 +0,0 @@
-import { vuepress } from 'eslint-config-vuepress'
-
-export default vuepress({
-    vue: {
-        overrides: {
-            'no-useless-assignment': 'off', // TODO: false positive in vue sfc
-        },
-    },
-})
